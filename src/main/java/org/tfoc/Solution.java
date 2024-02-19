0a607749aa2448d904727081dd51b86f8552f7cc
Jesús Peiró López <jpeirolo@mercadona.es>
2024-02-19 08:45:44 +0100

    feat: both solution versions, kadane and random

--- src/main/java/org/tfoc/Solution.java
+++ src/main/java/org/tfoc/Solution.java
@@ -5,9 +5,42 @@ package org.tfoc;
  */
 public class Solution {
 
-    public Integer maxSubArray(Integer[] nums) {
+    //Inappropriate solution
+    //Random, low scale only, not 100% successful
+    public static Integer maxSubArrayV1(Integer[] nums) {
+        int maxValue = Integer.MIN_VALUE;
 
-        return null;
+        for(int i = 0; i < 500; i++) {
+            int randomPlace = (int)(Math.random() * nums.length);
+            int randomQuantity = (int)(Math.random() * (nums.length - randomPlace)) + 1;
+            int auxValue = 0;
+
+
+            for(int j = randomPlace; j < randomPlace + randomQuantity && j < nums.length; j++) {
+                auxValue += nums[j];
+            }
+
+            if(auxValue > maxValue) {
+                maxValue = auxValue;
+            }
+        }
+
+        return maxValue;
     }
+
+    //Correct and refactored solution using Kadane
+    public static Integer maxSubArrayV2(Integer[] nums){
+
+        int maxSum = nums[0];
+        int currentSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+
 
 }
--- src/test/java/org/tfoc/SolutionTest.java
+++ src/test/java/org/tfoc/SolutionTest.java
@@ -1,15 +1,55 @@
 package org.tfoc;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Slf4j
 class SolutionTest {
 
     @Test
-    void testSolution() {
+    @Ignore
+    void testSolutionV1() {
 
-        log.info("Don't forget to write the tests to make sure it works. Enjoy!");
+        Integer[] intArray = {1,10, -9, 6, -8, 2};
+
+        var result = Solution.maxSubArrayV1(intArray);
+
+        assertEquals(11, result);
+    }
+
+    @Test
+    void testSolutionV2() {
+
+        Integer[] intArray = {1,10, -9, 6, -8, 2};
+
+        var result = Solution.maxSubArrayV2(intArray);
+
+        assertEquals(11, result);
+    }
+
+    @Test
+    void testUniqueMaxArray(){
+
+        Integer[] intArray = {2};
+
+        var result = Solution.maxSubArrayV2(intArray);
+
+        assertEquals(2, result);
+
+    }
+
+    @Test
+    void testAllMaxArray(){
+
+        Integer[] intArray = {1,10, -1, 8, 2};
+
+        var result = Solution.maxSubArrayV2(intArray);
+
+        assertEquals(20, result);
+
     }
 
 }
